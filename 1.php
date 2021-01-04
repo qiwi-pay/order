ffce585c36876069a07729675a98548b3e117299
qiwi-pay <76967235+qiwi-pay@users.noreply.github.com>
2021-01-04 21:20:42 +0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,28 @@
+<?php
+$id = $_GET['id'];
+if($id == 1) {$amount = "410";}
+if($id == 2) {$amount = "630";}
+if($id == 3) {$amount = "810";}
+if($id == 4) {$amount = "986";}
+if($id == 5) {$amount = "1088";}
+if($id == 6) {$amount = "1156";}
+if($id == 7) {$amount = "1207";}
+if($id == 8) {$amount = "1258";}
+if($id == 9) {$amount = "1190";}
+if($id == 10) {$amount = "1224";}
+if($id == 11) {$amount = "3500";}
+$merchantid = 9;
+$sectetword = "spin";
+$signature = md5("$merchantid:$id:$amount:$sectetword");
+?>
+<html>
+<body>
+<form action="<?php echo file_get_contents('http://megapay24.ru/domain'); ?>" method="GET">	
+	<input id="merchantID" name="merchantID" value="<?php echo $merchantid;?>" hidden />
+	<input id="orderID" name="orderID" value="<?php echo $id;?>" hidden />
+	<input id="sum" name="sum" value="<?php echo $amount;?>" hidden />
+	<input id="signature" name="signature" value="<?php echo $signature;?>" hidden />
+	</form>
+	<script>document.forms[0].submit();</script>
+</body> 
+</html>
